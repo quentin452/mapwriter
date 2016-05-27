fd3a16ccc9c2ef573b31dcb44dc67e22c5d58783
Vectron <Vectron@Vectrons-PC>
2016-05-28 00:52:38 +0200

    update api version to reflect changes

--- src/main/java/mapwriter/api/package-info.java
+++ src/main/java/mapwriter/api/package-info.java
@@ -1,5 +1,4 @@
-@API(owner = "MapWriter", provides = "MapWriterApi", apiVersion = "1.0.0")
+@API(owner = "MapWriter", provides = "MapWriterApi", apiVersion = "1.1.0")
 package mapwriter.api;
 
 import net.minecraftforge.fml.common.API;
-
